304f85de5e9db42586f8200577ed3162a94cd061
koosco <rnxogud123@naver.com>
2024-08-29 19:03:26 +0900

    [feat] goorm edu 크롤링 기능 수정
    * 로깅 추가

--- goorm/goorm-scraping.py
+++ goorm/goorm-scraping.py
@@ -1,3 +1,6 @@
+import logging
+
+from common.const import USER_AGENT
 from const.page import GOORM_URL
 from const.goorm_const import CATEGORIES, PAGE_NUM, PAGE_DETAIL_PREFIX_URL, SAVED_CSV_PATH, IMG_SAVED_PATH, \
     SAVED_JSON_PATH
@@ -6,6 +9,10 @@ from bs4 import BeautifulSoup
 import requests
 import base64
 import pandas as pd
+from logging import info
+
+logging.basicConfig(filename='goorm-scraping.log', filemode='w', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def preprocess_price(price: str):
@@ -35,29 +42,36 @@ def save_img(img, id: int):
 
 courses = []
 row_id = 1
-
+headers = {
+    'User-Agent': USER_AGENT
+}
 for page_num in range(1, PAGE_NUM):
-    html = requests.get(get_page_format(GOORM_URL, category=CATEGORIES[0], page_num=page_num)).text
+    page_url = get_page_format(GOORM_URL, category=CATEGORIES[0], page_num=page_num)
+    html = requests.get(page_url, headers=headers).text
     soup = BeautifulSoup(html, 'html.parser')
     cards = soup.select('.card')
     for card in cards:
         img_source_path = card.select_one('._3PxZMG._1bYAeB')['data-src']
         img_path = save_img(img_source_path, row_id)
-        course = {
-            'id': row_id,
-            'title': card.select_one('.card-title').text,
-            'image_url': img_path,
-            'instructor': card.select_one('._2q_4L7').text,
-            'price': preprocess_price(card.select_one('._1zPZlD').text),
-            'rating': float(card.select_one('._2KWt9f').text),
-            'lecture_url': PAGE_DETAIL_PREFIX_URL + card.parent['href']
-        }
+
+        try:
+            course = {
+                'id': row_id,
+                'title': card.select_one('.card-title').text,
+                'image_url': img_path,
+                'instructor': card.select_one('._2q_4L7').text,
+                'price': preprocess_price(card.select_one('._1zPZlD').text),
+                'rating': float(card.select_one('._2KWt9f').text),
+                'lecture_url': PAGE_DETAIL_PREFIX_URL + card.parent['href']
+            }
+            courses.append(course)
+            info(f'{row_id} saved')
+        except Exception:
+            info('[Exception occur]')
+            info(row_id)
+            info(card.select_one('.card-title').text)
+
         row_id += 1
-        courses.append(course)
-        print('id', course['id'])
-        print('title', course['title'])
-        print('image_url', course['image_url'])
-        print()
 
 print('search complete')
 df = pd.DataFrame(courses)
